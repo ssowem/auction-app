2e5880962e6076950203abfc0db73a218cc1f4b5
lsyeon0825@gmail.com <lsyeon0825@gmail.com>
2024-08-05 00:00:21 +0900

    feat: 로그인,회원가입 Route 추가

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import LoginForm from 'pages/LoginPage';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
+import SignupForm from 'pages/SignupPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<HomePage />}/>
-        {/* <Route path="/login" element={<LoginForm />}/> */}
-
+        <Route path="/login" element={<LoginForm />}/>
+        <Route path="/join" element={<SignupForm />}/>
       </Routes>
     </Router>
 
